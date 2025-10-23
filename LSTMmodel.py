31c0bf7eb25a9f5bc770f3ff64950969cf24f529
Maiko-Rigby <michaelrigby28@gmail.com>
2025-10-23 21:57:10 +0100

    Completed LSTM and created compareModels.py

--- LSTMmodel.py
+++ LSTMmodel.py
@@ -1,11 +1,4 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.linear_model import Ridge, Lasso
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import warnings
 import torch
