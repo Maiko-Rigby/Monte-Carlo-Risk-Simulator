b2005be0c7a2f95706e4266fbc0f81cee07d3493
Maiko-Rigby <michaelrigby28@gmail.com>
2025-10-25 23:47:14 +0100

    Update AWSSetup.py
    Created AWSSetup and init function

--- AWSSetup.py
+++ AWSSetup.py
@@ -10,3 +10,25 @@ from sagemaker.pytorch import PyTorch
 from sagemaker import get_execution_role
 import io
 import tarfile
+
+class AWSSetup:
+    def __init__(self, region = "eu-north-1"):
+        
+        self.region = region
+        self.s3_client = boto3.client('s3',region_name = region)
+        self.s3_resource = boto3.resource('s3',region_name = region)
+        self.iam_client = boto3.client('iam',region_name = region)
+        self.sagemaker_client = boto3.client('sagemaker',region_name = region)
+
+        try:
+            self.sagemaker_session =  sagemaker.Session()
+            self.role = get_execution_role()
+        except:
+            self.sagemaker_session = sagemaker.Session()
+            self.role = None
+            print(f'Not in SageMaker environment')
+
+        print("AWS Setup Complete")
+        print(f"Region: {self.region}")
+        print(f"Role: {self.role}")
+
